21fc2d4b1728548c4b168ae24b292f6d2202e115
LitanMolla <litanmolla9@gmail.com>
2025-06-17 03:00:50 +0600

    logo and nav active menu add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
                 <div className="md:w-1/2 space-y-2.5 md:text-start text-center">
                     <h2 className='text-3xl text-gray-300 font-medium'>Hi, I'm</h2>
                     {/* <h1 className='text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-50 to-cyan-600'>Litan Molla</h1> */}
-                    <h1 className='text-6xl font-bold  text-cyan-500'>Litan Molla</h1>
+                    <h1 className='lg:text-7xl md:text-6xl text-5xl font-bold  text-cyan-500'>Litan Molla</h1>
                     <h2 className='text-3xl text-gray-300 font-medium'>Frontend Developer</h2>
                     <p className='text-gray-400 text-base px-10 md:px-0'> I'm a passionate Frontend Developer dedicated to crafting modern, responsive, and user-centric web applications. I specialize in React and Tailwind CSS, blending clean design with smooth functionality to deliver exceptional user experiences.</p>
                     <button className='bg-cyan-500 px-5 py-3 rounded-lg cursor-pointer text-gray-900 text-lg font-semibold duration-300 hover:bg-cyan-600 mt-3 flex gap-2 items-center mx-auto md:mx-0'>Download Resume <FaDownload/></button>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,14 +3,13 @@ import { Link as HLink } from 'react-scroll';
 import { FaBarsStaggered } from "react-icons/fa6";
 import { FaWindowClose } from "react-icons/fa";
 import { useState } from 'react';
+import logo from '../assets/images/logo.webp'
 
 const Nav = () => {
     const [menu, setMenu] = useState(false);
 
     function menuHandle() {
         setMenu(!menu);
-        console.log('clicked');
-
     }
     return (
         <>
@@ -18,32 +17,35 @@ const Nav = () => {
                 <nav className='max-w-7xl mx-auto px-4 sm:px-5 md:px-6 lg:px-8'>
                     <div className='flex justify-between items-center'>
                         <div>
-                            <Link to={'/'}><span className='text-4xl text-gray-50 font-bold'>Litan</span></Link>
+                            <Link to={'/'}><span className='text-4xl text-cyan-500 font-bold w-12 block'>
+                                <img src={logo} alt="Logo" className='w-full' />
+                                </span></Link>
                         </div>
                         {/* Lg Menu */}
                         <div className='hidden md:block'>
                             <ul className='flex font-medium text-gray-50 space-x-8 text-lg'>
-                                <HLink to='hero' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>Home</li></HLink>
-                                <HLink to='about' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>About</li></HLink>
-                                <HLink to='skills' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>Skills</li></HLink>
-                                <HLink to='portfolio' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>Portfolio</li></HLink>
-                                <HLink to='contact' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>Contact</li></HLink>
+                                <HLink to='hero' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-156}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>Home</li></HLink>
+                                <HLink to='about' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-156}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>About</li></HLink>
+                                <HLink to='skills' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-156}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>Skills</li></HLink>
+                                <HLink to='portfolio' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-156}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>Portfolio</li></HLink>
+                                <HLink to='contact' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-233}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>Contact</li></HLink>
                             </ul>
                         </div>
                         {/* Menu bar for mobile */}
                         <div className='text-gray-50 text-2xl md:hidden'>
-                            <button onClick={menuHandle} className='cursor-pointer'>{menu ? <FaWindowClose /> : <FaBarsStaggered />}</button>
+                            <button onClick={menuHandle} className='cursor-pointer '>{menu ?  '' : <FaBarsStaggered />}</button>
                         </div>
                     </div>
                     {/* Mobile Menu */}
                     {
-                        menu && <div className='md:hidden absolute top-0 bg-gray-900/95 h-screen left-0 w-1/3 text-center z-20'>
+                        menu && <div className='md:hidden absolute top-0 bg-gray-900/95 h-screen left-0 w-full text-center z-30'>
+                            <button onClick={menuHandle} className='cursor-pointer text-2xl text-gray-50 absolute top-5 right-5'>{menu ? <FaWindowClose /> : '' }</button>
                             <ul className='flex flex-col font-medium text-gray-50 space-y-2 text-lg justify-center h-screen'>
-                                <HLink to='hero' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>Home</li></HLink>
-                                <HLink to='about' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>About</li></HLink>
-                                <HLink to='skills' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>Skills</li></HLink>
-                                <HLink to='portfolio' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>Portfolio</li></HLink>
-                                <HLink to='contact' smooth={true} duration={500} ><li className='duration-300 hover:text-cyan-500'>Contact</li></HLink>
+                                <HLink to='hero' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-124}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>Home</li></HLink>
+                                <HLink to='about' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-124}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>About</li></HLink>
+                                <HLink to='skills' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-124}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>Skills</li></HLink>
+                                <HLink to='portfolio' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-124}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>Portfolio</li></HLink>
+                                <HLink to='contact' smooth={true} duration={500} spy={true} activeClass='text-cyan-500' offset={-124}><li className='duration-300 hover:text-cyan-500 cursor-pointer'>Contact</li></HLink>
                             </ul>
                         </div>
                     }
